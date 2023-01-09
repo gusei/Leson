5ed0928ee46a39701e2a6f72ebe23074b1e3799f
Администратор <@WIN-R3T3CJ5EAGK>
2023-01-09 17:22:09 +0300

    First Lesson

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ win.title('Введите домашний адресс')
 frm_from=tk.Frame(relief=tk.SUNKEN,borderwidth=3)
 frm_from.pack()
 
+lbl_firs_name=tk.Label(master=frm_from,text="Имя")
+ent_first_name=tk.Entry(master=frm_from,width=50)
+
 win.mainloop()
