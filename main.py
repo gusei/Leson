8f6102ad198b0dd3cfae2aa63f6795e5fb4184fc
Администратор <@WIN-R3T3CJ5EAGK>
2023-01-10 17:17:55 +0300

    First Lesson

--- main.py
+++ main.py
@@ -23,6 +23,26 @@ ent_address1=tk.Entry(master=frm_from,width=50)
 lbl_address1.grid(row=2,column=0,sticky=tk.E)
 ent_address1.grid(row=2,column=1)
 
+lbl_address2=tk.Label(master=frm_from,text='Адрес 2:')
+ent_address2=tk.Entry(master=frm_from,width=50)
+
+lbl_address2.grid(row=3,column=0,sticky=tk.E)
+ent_address2.grid(row=3,column=1)
+
+lbl_city=tk.Label(master=frm_from,text='Город')
+ent_city=tk.Entry(master=frm_from,width=50)
+
+lbl_city.grid(row=4,column=0,sticky=tk.E)
+ent_city.grid(row=4,column=1)
+
+lbl_postal_code=tk.Label(master=frm_from,text='Почтовый индекс')
+ent_postal_code=tk.Entry(master=frm_from,width=50)
+
+lbl_postal_code.grid(row=5,column=0,sticky=tk.E)
+ent_postal_code.grid(row=5,column=1)
+
+
+
 
 
 win.mainloop()
